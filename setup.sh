64d1856b6dabc74ce83252dd10ac6cc08303b98e
Ahmed Gharib <a.gharib89@yahoo.com>
2021-01-15 21:35:31 +0200

    update the app to allow create delete update books

--- setup.sh
+++ setup.sh
@@ -3,5 +3,7 @@ pip3 install flask_cors
 pip3 install flask --upgrade
 pip3 uninstall flask-socketio -y
 service postgresql start
-su - postgres bash -c "psql < /home/workspace/backend/setup.sql"
-su - postgres bash -c "psql bookshelf < /home/workspace/backend/books.psql"
+su - postgres bash -c "psql < /d/1- Study/In-progress/AWDND/flask_setup_example/backend
+setup.sql"
+su - postgres bash -c "psql bookshelf < /d/1- Study/In-progress/AWDND/flask_setup_example/backend
+/books.psql"
